ac1ff280de12731078ee2dd2d2c0d290f55de55a
Harshal <88491916+Pixel-Crafter@users.noreply.github.com>
2025-01-31 21:08:07 +0530

    Navbar

---
+++ constants/index.ts
@@ -0,0 +1,17 @@
+export const Socials = [
+    {
+        name: "Discord",
+        src: "/discord.svg",
+        url: "https://discord.gg/9MXRvC37SP"
+    },
+    {
+        name: "GitHub",
+        src: "/github-mark-white.svg",
+        url: "https://github.com/Pixel-Crafter"
+    },
+    {
+        name: "Instagram",
+        src: "/instagram.svg",
+        url: "https://www.instagram.com/harshal_2209",
+    },
+];
