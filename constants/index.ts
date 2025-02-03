587f305ea0183232c657559674ac663ec7b72e5e
Harshal <88491916+Pixel-Crafter@users.noreply.github.com>
2025-02-03 21:07:07 +0530

    Major Done

--- constants/index.ts
+++ constants/index.ts
@@ -18,24 +18,100 @@ export const Socials = [
 
 export const Projects = [
     {
-        name: "Project 1",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-        src: "/", alt: "Project Thumbnail"
+        name: "Personal Portfolio",
+        text: "Current Portfolio made using NextJs and Tailwind CSS",
+        src: "/portfolio.jpg", alt: "Project Thumbnail"
 
     },
     {
-        name: "Project 2",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-        src: "/", alt: "Project Thumbnail"
+        name: "Flappy Bird Game",
+        text: "Classic Flappy Bird Game with Score Counter",
+        src: "/flappyBird.jpg", alt: "Project Thumbnail"
     },
     {
-        name: "Project 3",
+        name: "Vehicle Parking Management",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-        src: "/", alt: "Project Thumbnail"
+        src: "/vpmsDashboard.JPG", alt: "Project Thumbnail"
     },
     {
-        name: "Project 4",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-        src: "/", alt: "Project Thumbnail"
+        name: "Spotify Clone",
+        text: "Spotify Clone using Tailwind CSS and Stripe 'Work in Progress'",
+        src: "/spotifyClone.jpg", alt: "Project Thumbnail"
+    },
+];
+
+export const SkillData = [
+    {
+        name: "Html 5",
+        Image: "/html.png",
+        width: 80,
+        height: 80,
+    },
+    {
+        name: "CSS",
+        Image: "/css.png",
+        width: 80,
+        height: 80,
+    },
+    {
+        name: "JavaScript",
+        Image: "/js.png",
+        width: 65,
+        height: 65,
+    },
+    {
+        name: "Tailwind Css",
+        Image: "/tailwind.png",
+        width: 80,
+        height: 80,
+    },
+    {
+        name: "React",
+        Image: "/react.png",
+        width: 80,
+        height: 80,
+    },
+    {
+        name: "Redux",
+        Image: "/redux.png",
+        width: 80,
+        height: 80,
+    },
+    
+    {
+        name: "TypeScript",
+        Image: "/ts.png",
+        width: 80,
+        height: 80,
+    },
+    {
+        name: "Next js 13",
+        Image: "/next.png",
+        width: 80,
+        height: 80,
+    },
+    {
+        name: "Framer Motion",
+        Image: "/framer.png",
+        width: 80,
+        height: 80,
+    },
+    {
+        name: "Stripe Payment",
+        Image: "/stripe.webp",
+        width: 80,
+        height: 80,
+    },
+    {
+        name: "Node js",
+        Image: "/node-js.png",
+        width: 80,
+        height: 80,
+    },
+    {
+        name: "Mongo db",
+        Image: "/mongodb.png",
+        width: 40,
+        height: 40,
     },
 ];
